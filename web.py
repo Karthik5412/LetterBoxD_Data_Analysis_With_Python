19e33bbe573771138053f4d26f63936f1ef02bf8
Karthik5412 <kkuricharlapati@gmail.com>
2026-01-29 21:41:58 +0530

    going on

--- web.py
+++ web.py
@@ -5,6 +5,7 @@ import joblib
 
 countries_list = joblib.load('countries_list.plk')
 unique_actors = joblib.load('unique_actors.plk') 
+geners = joblib.load('gener_list.plk')
 
 df = pd.read_csv('cleaned.csv')
 
@@ -25,6 +26,22 @@ actors = st.sidebar.multiselect(
     options=unique_actors
 )
 
+gener = st.sidebar.multiselect(
+    'Select Gener',
+    options=geners
+)
+
 year = st.sidebar.slider('Select Year',df['year'].min(),df['year'].max())
 
+col1, col2 = st.columns([0.3,0.7])
+
+with col1:
+    fig = px.bar(df.head(20),x='revenue',y='title')
+    st.plotly_chart(fig)
+    
+with col2: 
+    fig = px.line(df.head(20),y='date_added',x='popularity')
+    st.plotly_chart(fig) 
+
+# px.histogram(data=df.head(20),x='revenue',y='title')
 st.write(df)
